0b27a78d7766abe624583aae848e76b0766fb11b
Yair Rosenschein <yairros@gmail.com>
2023-12-06 22:39:00 +0200

    5

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ app.use(
   cors({
     origin: [
   "http://localhost:3000",
-  // "https://adoptify-server-aucc.onrender.com"
+  "https://petz-bpm5.onrender.com"
 ],
     credentials: true,
   })
